20b5d3647f1219510d26016fc191b433e4b1aaa8
naga1979 <56697162+naga1979@users.noreply.github.com>
2022-09-28 09:37:22 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
-docker stop hello/newapp:1.1; 
-docker rm -f hello/newapp:1.1; 
-docker image rm -f hello/newapp:1.1; 
+ 
 cd /home/dockeradmin/build ; 
-docker build -t hello/newapp:1.1 .;
-docker run -it -p 3000:80 -d hello/newapp:1.1
+
+docker run -it -p 3000:80 -d hello/newapp:1.1;
+mkdir hellooooooo
